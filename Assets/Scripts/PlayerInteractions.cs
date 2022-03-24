72059a24ab758e67d3b959d5f6842ed2c061cf86
Tijn Flinterman <t.flinterman@sintlucasedu.nl>
2022-03-24 14:28:54 +0100

    Detection if bumping left or right for tree and zombie. + prefabs

--- Assets/Scripts/PlayerInteractions.cs
+++ Assets/Scripts/PlayerInteractions.cs
@@ -4,23 +4,81 @@ using UnityEngine;
 
 public class PlayerInteractions : MonoBehaviour
 {
-    void Update()
-    {
-        
-    }
+    private bool hasBumped;
+    private float dist;
+    [SerializeField] [Range(0f, 1f)] float distance = 0.75f;
+
     private void OnTriggerEnter(Collider coll)
     {
         if (coll.gameObject.tag == "Zombie")
         {
-            float dist = Vector3.Distance(transform.position, coll.transform.position);
-            if (dist > 0.8f)
+            dist = Vector3.Distance(transform.position, coll.transform.position);
+            if (dist > distance)
             {
                 Debug.Log("far hit, distance is: " + dist);
+
+                if (transform.position.x < coll.transform.position.x)
+                {
+                    Debug.Log("Left");
+                    if (hasBumped == false)
+                    {
+                        BumpLeft();
+                        hasBumped = true;
+                    }
+                }
+                if (transform.position.x > coll.transform.position.x)
+                {
+                    Debug.Log("Right");
+                    if (hasBumped == false)
+                    {
+                        BumpRight();
+                        hasBumped = true;
+                    }
+                }
             }
-            if (dist < 0.8f)
+            if (dist < distance)
             {
                 Debug.Log("close hit, distance is: " + dist);
+
+                // add player death
+
             }
         }
+
+        if (coll.gameObject.tag == "Tree")
+        {
+            if (transform.position.x < coll.transform.position.x)
+            {
+                Debug.Log("Left");
+                if (hasBumped == false)
+                {
+                    BumpLeft();
+                    hasBumped = true;
+                }
+            }
+            if (transform.position.x > coll.transform.position.x)
+            {
+                Debug.Log("Right");
+                if (hasBumped == false)
+                {
+                    BumpRight();
+                    hasBumped = true;
+                }
+            }
+        }
+    }
+
+    void BumpLeft()
+    {
+    
+    }
+    void BumpRight()
+    {
+    
+    }
+
+    private void OnTriggerExit(Collider coll)
+    {
+        hasBumped = false;
     }
 }
