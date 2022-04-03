c6f0d980d4f1b225a39c294289e92fca4567eed6
Tijn Flinterman <t.flinterman@sintlucasedu.nl>
2022-04-03 20:55:24 +0200

    new

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+    public float transitionTime;
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        animator.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
